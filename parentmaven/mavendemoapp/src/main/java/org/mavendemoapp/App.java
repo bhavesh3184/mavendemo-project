36a77528c28e64d88020e638c02ca11fd138da5e
bhavesh3184 <bhavesh3184@gmail.com>
2018-07-01 23:33:03 +0100

    initial commit

---
+++ parentmaven/mavendemoapp/src/main/java/org/mavendemoapp/App.java
@@ -0,0 +1,13 @@
+package org.mavendemoapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
